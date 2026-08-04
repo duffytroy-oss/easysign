78a1cb5c1f67653759f5e7a4c24bae5cfbcdd603
Troy Duffy <duffytroy@gmail.com>
2026-08-03 19:05:05 -0600

    Initial EasySign website

---
+++ sign.js
@@ -0,0 +1,36 @@
+const params = new URLSearchParams(window.location.search);
+
+const token = params.get("token");
+
+if(!token){
+
+    document.body.innerHTML="Missing token.";
+
+    throw "";
+
+}
+
+fetch(
+"https://ltatudiuhozwbufqybxd.supabase.co/functions/v1/sign-document?token=" + token
+)
+
+.then(r=>r.json())
+
+.then(data=>{
+
+document.getElementById("tenantName").innerText=
+data.tenant_name;
+
+document.getElementById("leasePDF").src=
+data.pdf_url;
+
+})
+
+.catch(error=>{
+
+console.log(error);
+
+document.body.innerHTML=
+"Unable to load document.";
+
+});
